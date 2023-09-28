10c9c87d03ec0fb23045d5c1e56368fc9d3bb72f
Ankur <ankurgoswami718@gmail.com>
2023-09-28 13:00:09 +0530

    deploy

--- server.js
+++ server.js
@@ -5,8 +5,6 @@ import userRoute from "./routes/userRoute.js";
 import adminRoute from "./routes/adminRoute.js";
 import doctorRoute from "./routes/doctorsRoute.js";
 
-import path from "path";
-
 dotenv.config();
 
 const app = express();
@@ -15,13 +13,6 @@ app.use(express.json());
 app.use("/api/user", userRoute);
 app.use("/api/admin", adminRoute);
 app.use("/api/doctor", doctorRoute);
-
-if (process.env.NODE_ENV === "production") {
-    app.use("/", express.static("client/build"));
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client/build/index.html"));
-    });
-}
 
 const port = process.env.PORT || 5000;
 app.get("/", (req, res) => {
